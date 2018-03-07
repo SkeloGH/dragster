ce960a73d235b39da76bc2ba6fdffab7539406df
Aaron_GH <aaron@rigs.com.mx>
2018-03-07 13:10:18 -0600

    removed placeholder file and simplified initialization

--- assets/game/js/dragstrip.js
+++ assets/game/js/dragstrip.js
@@ -1,17 +1,109 @@
-define("dragstrip", ['stage', 'utils', 'playercar','gearbox'], function(){
+function DragStrip(config){
   "use strict";
-  var cfg, stage;
-  cfg = {
-    screen_width: window.document.body.scrollWidth,
-    screen_height: window.document.body.scrollHeight,
-    renderer: Phaser.AUTO,
-    element: '',
+  var characters, game, playerCam, background,
+    enemy, player, cursors, shift_text;
+  var self = this;
+  this.cfg = {
+    screen_width: config.screen_width || window.document.body.scrollWidth,
+    screen_height: config.screen_height || window.document.body.scrollHeight,
+    renderer: config.renderer || Phaser.AUTO,
+    element: config.element || '',
     world: {
-      width: metersToPx(402.34),
-      height: 512
+      width: config.world.width || metersToPx(402.34),
+      height: config.world.height || 512
+    },
+    handlers: {
+      preload: preload,
+      create: create,
+      update: update,
+      render: render
+    },
+  };
+
+  self.states = {
+    timer: {
+      start : 0,
+      finish : 0
     }
   };
 
-  stage = new Stage(cfg);
-  stage.init();
-});
+  this.init = function init(){
+    var cfg = self.cfg;
+    game = new Phaser.Game(cfg.screen_width,
+      cfg.screen_height, cfg.renderer,
+      cfg.element, cfg.handlers);
+  };
+
+  function preload() {
+    game.stage.backgroundColor = '#dedede';
+    game.load.image('BG__dragstrip', '/assets/game/img/bg/dragstrip.png');
+    game.load.image('CAR__player', '/assets/game/img/cars/phantom/car.png');
+    game.load.image('CAR__enemy', '/assets/game/img/cars/phantom/car.png');
+    game.time.advancedTiming = true;
+    // window.addEventListener('resize', function () {
+    //   game.scale.setGameSize(window.document.body.scrollWidth, window.document.body.scrollHeight);
+    // });
+  }
+
+  function create() {
+    var cfg = self.cfg;
+    shift_text  = new Phaser.Text(game, 0, 0, 'N', {color: '#000'});
+    background  = game.add.tileSprite(0, 0, cfg.world.width, cfg.world.height, 'BG__dragstrip');
+    cursors     = game.input.keyboard.createCursorKeys();
+    characters  = {
+      'enemy': game.add.sprite(0, 239, 'CAR__enemy')
+    };
+    player  = new PlayerCar({
+      sprite: game.add.sprite(0, 377, 'CAR__player'),
+      gearbox: new Gearbox({ shifts: 6, shiftingDelay: 100 }),
+      topSpeed: kmHrToPxSec(150),
+      topRwdSpeed: kmHrToPxSec(-50)
+    });
+
+    game.world.setBounds(0, 0, cfg.world.width, cfg.world.height);
+    game.physics.arcade.enable(player.car);
+    game.camera.follow(player.car);
+  }
+
+  function update() {
+    checkTiming();
+    player.update(cursors);
+  }
+
+  function render(){
+    game.debug.cameraInfo(game.camera, 32, 32);
+    game.debug.spriteCoords(player.car, 32, 500);
+    game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
+  }
+
+  function checkTiming(){
+    var cfg = self.cfg;
+    var carPos, worldLimit, startTime, finishTime;
+    carPos     = player.car.body.x;
+
+    if (carPos > 0){
+      (function check(){
+        worldLimit = cfg.world.width;
+        startTime  = self.states.timer.start;
+        finishTime = self.states.timer.finish;
+
+        if (carPos < worldLimit && !startTime && !finishTime) {
+            timer('start');
+        }
+        if (carPos >= worldLimit && !!startTime && !finishTime) {
+            timer('finish');
+        }
+      })();
+    }
+  }
+
+  function timer(action){
+    var total;
+    self.states.timer[action] = Date.now();
+    if (action == 'finish') {
+      total = self.states.timer.finish - self.states.timer.start;
+      console.log(total);
+      return total;
+    }
+  }
+}
--- assets/game/js/main.js
+++ assets/game/js/main.js
@@ -1,1 +1,17 @@
-require(['dragstrip']);
+define("main", ['dragstrip', 'utils', 'playercar','gearbox'], function(){
+  "use strict";
+  var cfg, stage;
+  cfg = {
+    screen_width: window.document.body.scrollWidth,
+    screen_height: window.document.body.scrollHeight,
+    renderer: Phaser.AUTO,
+    element: '',
+    world: {
+      width: metersToPx(402.34),
+      height: 512
+    }
+  };
+
+  stage = new DragStrip(cfg);
+  stage.init();
+});
