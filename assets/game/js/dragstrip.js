95088b8fc94338787c8363dd1419852e6776843f
Aaron_GH <aaron@rigs.com.mx>
2018-03-07 14:03:36 -0600

    changed, dragstrip functions are now class methods

--- assets/game/js/dragstrip.js
+++ assets/game/js/dragstrip.js
@@ -1,103 +1,106 @@
 function DragStrip(config){
   "use strict";
-  var characters, game, playerCam, background,
-    enemy, player, cursors, shift_text;
-  var self = this;
-  this.cfg = {
-    screen_width: config.screen_width || window.document.body.scrollWidth,
-    screen_height: config.screen_height || window.document.body.scrollHeight,
-    renderer: config.renderer || Phaser.AUTO,
-    element: config.element || '',
-    world: {
-      width: config.world.width || metersToPx(402.34),
-      height: config.world.height || 512
-    },
-    handlers: {
-      preload: preload,
-      create: create,
-      update: update,
-      render: render
-    },
-  };
-
-  self.states = {
-    timer: {
-      start : 0,
-      finish : 0
-    }
-  };
+  var self        = this;
+  this.u_cfg      = config;
+  this.game       = null;
+  this.cursors    = null;
+  this.characters = {};
+  this.player     = null;
+  this.states     = {};
+  this.cfg        = {};
 
   this.init = function init(){
-    var cfg = self.cfg;
-    game = new Phaser.Game(cfg.screen_width,
-      cfg.screen_height, cfg.renderer,
-      cfg.element, cfg.handlers);
+    var u_cfg = self.u_cfg;
+    self.cfg   = {
+      screen_width: u_cfg.screen_width || 800,
+      screen_height: u_cfg.screen_height || 600,
+      renderer: u_cfg.renderer || Phaser.AUTO,
+      element: u_cfg.element || '',
+      world: {
+        width: u_cfg.world.width || metersToPx(402.34),
+        height: u_cfg.world.height || 512
+      },
+      handlers: {
+        preload: self.preload,
+        create: self.create,
+        update: self.update,
+        render: self.render
+      },
+    };
+    self.states = {
+      timer: {
+        start : 0,
+        finish : 0
+      }
+    };
+    self.game = new Phaser.Game(self.cfg.screen_width,
+      self.cfg.screen_height, self.cfg.renderer,
+      self.cfg.element, self.cfg.handlers);
   };
 
-  function preload() {
-    game.stage.backgroundColor = '#dedede';
-    game.load.image('BG__dragstrip', '/assets/game/img/bg/dragstrip.png');
-    game.load.image('CAR__player', '/assets/game/img/cars/phantom/car.png');
-    game.load.image('CAR__enemy', '/assets/game/img/cars/phantom/car.png');
-    game.time.advancedTiming = true;
+  this.preload = function preload() {
+    self.game.stage.backgroundColor = '#dedede';
+    self.game.load.image('BG__dragstrip', '/assets/game/img/bg/dragstrip.png');
+    self.game.load.image('CAR__player', '/assets/game/img/cars/phantom/car.png');
+    self.game.load.image('CAR__enemy', '/assets/game/img/cars/phantom/car.png');
+    self.game.time.advancedTiming = true;
     // window.addEventListener('resize', function () {
     //   game.scale.setGameSize(window.document.body.scrollWidth, window.document.body.scrollHeight);
     // });
-  }
+  };
 
-  function create() {
-    var cfg = self.cfg;
-    shift_text  = new Phaser.Text(game, 0, 0, 'N', {color: '#000'});
-    background  = game.add.tileSprite(0, 0, cfg.world.width, cfg.world.height, 'BG__dragstrip');
-    cursors     = game.input.keyboard.createCursorKeys();
-    characters  = {
-      'enemy': game.add.sprite(0, 239, 'CAR__enemy')
+  this.create = function create() {
+    var cfg         = self.cfg;
+    var shift_text  = new Phaser.Text(self.game, 0, 0, 'N', {color: '#000'});
+    var background  = self.game.add.tileSprite(0, 0, cfg.world.width, cfg.world.height, 'BG__dragstrip');
+    self.cursors    = self.game.input.keyboard.createCursorKeys();
+    self.characters = {
+      'enemy': self.game.add.sprite(0, 239, 'CAR__enemy')
     };
-    player  = new PlayerCar({
-      sprite: game.add.sprite(0, 377, 'CAR__player'),
+    self.player  = new PlayerCar({
+      sprite: self.game.add.sprite(0, 377, 'CAR__player'),
       gearbox: new Gearbox({ shifts: 6, shiftingDelay: 100 }),
       topSpeed: kmHrToPxSec(150),
       topRwdSpeed: kmHrToPxSec(-50)
     });
 
-    game.world.setBounds(0, 0, cfg.world.width, cfg.world.height);
-    game.physics.arcade.enable(player.car);
-    game.camera.follow(player.car);
-  }
+    self.game.world.setBounds(0, 0, cfg.world.width, cfg.world.height);
+    self.game.physics.arcade.enable(self.player.car);
+    self.game.camera.follow(self.player.car);
+  };
 
-  function update() {
-    checkTiming();
-    player.update(cursors);
-  }
+  this.update = function update() {
+    self.checkTiming();
+    self.player.update(self.cursors);
+  };
 
-  function render(){
-    game.debug.cameraInfo(game.camera, 32, 32);
-    game.debug.spriteCoords(player.car, 32, 500);
-    game.debug.text(game.time.fps || '--', 2, 14, "#00ff00");
-  }
+  this.render = function render(){
+    self.game.debug.cameraInfo(self.game.camera, 32, 32);
+    self.game.debug.spriteCoords(self.player.car, 32, 500);
+    self.game.debug.text(self.game.time.fps || '--', 2, 14, "#00ff00");
+  };
 
-  function checkTiming(){
-    var cfg = self.cfg;
-    var carPos, worldLimit, startTime, finishTime;
-    carPos     = player.car.body.x;
+  this.checkTiming = function checkTiming(){
+    var carPos, worldLimit, startTime, finishTime, shouldCount, hasStarted, hasFinished;
+    carPos     = self.player.car.body.x;
+    worldLimit = self.cfg.world.width;
+    startTime  = self.states.timer.start;
+    finishTime = self.states.timer.finish;
+    hasStarted = carPos < worldLimit && !startTime && !finishTime;
+    hasFinished = carPos >= worldLimit && !!startTime && !finishTime;
+    shouldCount = carPos > 0 || startTime > 0;
 
-    if (carPos > 0){
-      (function check(){
-        worldLimit = cfg.world.width;
-        startTime  = self.states.timer.start;
-        finishTime = self.states.timer.finish;
-
-        if (carPos < worldLimit && !startTime && !finishTime) {
-            timer('start');
-        }
-        if (carPos >= worldLimit && !!startTime && !finishTime) {
-            timer('finish');
-        }
-      })();
+    if (shouldCount){
+      if (hasStarted) {
+        self.timer('start');
+      }
+      if (hasFinished) {
+        self.timer('finish');
+      }
     }
-  }
+  };
 
-  function timer(action){
+  this.timer = function timer(action){
     var total;
     self.states.timer[action] = Date.now();
     if (action == 'finish') {
@@ -105,5 +108,7 @@ function DragStrip(config){
       console.log(total);
       return total;
     }
-  }
+  };
+
+  return this;
 }
