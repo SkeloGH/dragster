1bdcb656bf46c4cfcb826f62722868dc85e6d03e
Aaron Gonzalez <SkeloGH@users.noreply.github.com>
2018-03-08 16:30:05 -0600

    changed functions to class definitions per #2 (#8)
    * changed functions to class definitions per #2
    
    * added CircleCI config

--- assets/game/js/gearbox.js
+++ assets/game/js/gearbox.js
@@ -1,37 +1,41 @@
-function Gearbox(config){
-  "use strict";
-  this._shifting    = null;
-  this._shifts      = {
-    "-1":"R", "0":"N",
-    "1":"1st", "2":"2nd",
-    "3":"3rd", "4":"4th",
-    "5":"5th", "6":"6th"
-  };
-  this.total_shifts   = config.max_gears;
-  this.shifting_delay = config.shifting_delay;
-  this.current_shift  = { value: 0, name: 'N'};
+/*jshint esversion: 6*/
+"use strict";
+export class Gearbox{
+  constructor(config){
+    this._shifting    = null;
+    this._shifts      = {
+      "-1":"R", "0":"N",
+      "1":"1st", "2":"2nd",
+      "3":"3rd", "4":"4th",
+      "5":"5th", "6":"6th"
+    };
+    this.total_shifts   = config.max_gears;
+    this.shifting_delay = config.shifting_delay;
+    this.current_shift  = { value: 0, name: 'N'};
+    return this;
+  }
 
-  this.getCurrentShift = function getCurrentShift() {
+  getCurrentShift() {
     return this.current_shift;
-  };
+  }
 
-  this.shiftUp = function shiftUp(){
+  shiftUp(){
     this._debounceShift(function(){
       return this.current_shift.value < this.total_shifts && this.current_shift.value++;
     });
 
     return this;
-  };
+  }
 
-  this.shiftDown = function shiftDown(){
+  shiftDown(){
     this._debounceShift(function(){
       return this.current_shift.value > -1 && this.current_shift.value--;
     });
 
     return this;
-  };
+  }
 
-  this._debounceShift = function _debounceShift(callback){
+  _debounceShift(callback){
     this._clearState();
     this._shifting = setTimeout(function(){
       callback.call(this);
@@ -39,15 +43,13 @@ function Gearbox(config){
       console.log(this.current_shift);
     }.bind(this), this.shifting_delay);
     return this;
-  };
+  }
 
-  this._clearState = function _clearState(){
+  _clearState(){
     if (this._shifting !== null) {
       clearTimeout(this._shifting);
       this._shifting = null;
     }
     return this;
-  };
-
-  return this;
+  }
 }
--- assets/game/js/utils.js
+++ assets/game/js/utils.js
@@ -1,11 +1,14 @@
-function metersToPx(meters){
-  // 1m == 100px
-  var meterAsPx = 1 * 100;
-  return (meters * meterAsPx) / 1;
-}
+/*jshint esversion:6*/
+export class Utils {
+  static metersToPx(meters){
+    // 1m == 100px
+    var meterAsPx = 1 * 100;
+    return (meters * meterAsPx) / 1;
+  }
 
-function kmHrToPxSec(kmHr){
-  // 1kmh == .28ms == 28pxs
-  var kmHrAsPxSec = 0.28 * 100;
-  return (kmHr * kmHrAsPxSec) / 1;
+  static kmHrToPxSec(kmHr){
+    // 1kmh == .28ms == 28pxs
+    var kmHrAsPxSec = 0.28 * 100;
+    return (kmHr * kmHrAsPxSec) / 1;
+  }
 }
