5704ee827493ee7cd96b7556b3a1b3cc0ddb1cb1
Aaron_GH <aaron@rigs.com.mx>
2018-03-09 12:52:56 -0600

    added, environment port support for deploying to heroku, and defined Procfile

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.use('/', express.static('views'));
 app.use('/assets', express.static('assets'));
 app.use('/dist', express.static('dist'));
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('http://127.0.0.1:3000');
 });
