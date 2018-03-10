65f947e2f811098d5ba82e18fff0f3e79e583689
Aaron_GH <aaron@rigs.com.mx>
2018-03-09 18:19:31 -0600

    added, basic message prototype

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-var express = require('express');
-var app = express();
+/*jshint esversion: 6*/
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const http_port = process.env.PORT || 3000;
 
 app.use('/', express.static('root'));
 app.use('/', express.static('views'));
 app.use('/assets', express.static('assets'));
 app.use('/dist', express.static('dist'));
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('http://127.0.0.1:3000');
+// app.listen(http_port, function () {
+//   console.log('http://127.0.0.1:'+http_port);
+// });
+http.listen(http_port, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('user.joined', function(msg){
+    console.log('message: ' + msg);
+    io.emit('user.joined', { for: 'everyone' });
+  });
 });
