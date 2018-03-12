b543e61ee2e3bb3ef4ad3c333cdbafa669d87f66
Aaron Gonzalez <SkeloGH@users.noreply.github.com>
2018-03-12 14:47:03 -0600

    Issue #13 (#19)
    * added, socketio support
    
    * added, basic message prototype
    
    * - changed, html spacing to tabs
    - added, socket.io connection
    - added, waiting room, used to keep users waiting in queue
    - added, matching room, used when another user joins the room
    - added, when a user leaves the room and the room has still one player, move the room into the waiting queue
    
    * added, enemy player movement into refresh loop based on the player's position

--- server.js
+++ server.js
@@ -1,11 +1,70 @@
-var express = require('express');
-var app = express();
+/*jshint esversion: 6*/
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const http_port = process.env.PORT || 3000;
+
+let waiting = [];
+let matched = [];
 
 app.use('/', express.static('root'));
 app.use('/', express.static('views'));
 app.use('/assets', express.static('assets'));
 app.use('/dist', express.static('dist'));
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('http://127.0.0.1:3000');
+
+http.listen(http_port, function(){
+  console.log('listening on *:3000');
 });
+
+io.on('connection', function(socket){
+  let uid = socket.handshake.query.uid;
+
+  join(uid, socket);
+
+  socket.on('disconnect', function(){
+    leave(uid);
+  });
+  socket.on('user.update', function(data){
+    io.to(data.room).emit('user.update', data);
+  });
+});
+
+const join = (uid, socket)=>{
+  let room = {};
+
+  if (waiting.length > 0) {
+    room = waiting.pop();
+    room.users.push(uid);
+    matched.push(room);
+  }else{
+    room.name = Date.now();
+    room.users = [uid];
+    waiting.push(room);
+  }
+
+  socket.join(room.name);
+  io.to(room.name).emit('user.joined', {id: uid, room: room.name});
+  console.log(uid+' user connected');
+};
+
+const leave = (uid) => {
+  // - Looks for the room with the given uid (inneficient lookup for now).
+  // - Removes the user from the room.
+  // - Moves the room back to the waiting queue.
+  matched.forEach(function(match_room, room_idx){
+    match_room.users.forEach(function(user, usr_idx, source){
+
+      if(user == uid){
+        source.splice(usr_idx, 1);
+        if (source.length > 0) {
+          waiting.push(match_room);
+          matched.splice(room_idx, 1);
+        }
+      }
+    });
+  });
+
+  console.log(uid+' user disconnected');
+};
